8f1aacb93b443db61dc5bc646715a376b21c285a
William Ting <williamdjting@gmail.com>
2022-06-16 03:41:46 -0700

    added app.get database ejs path

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+var app = express();
+  
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.get('/database', (req, res) => {
+  var data = { results: [2, 3, 4, 5, 6]};
+  res.render('pages/db',data);
+});
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
