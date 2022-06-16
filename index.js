3b82b5d234f499954693716b039add61d5007cc1
William Ting <williamdjting@gmail.com>
2022-06-16 03:56:43 -0700

    change

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 var app = express();
-  
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/database', (req, res) => {
-  var data = { results: [2, 3, 4, 5, 6]};
-  res.render('pages/db',data);
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/database", (req, res) => {
+  var data = { results: [2, 3, 4, 5, 6] };
+  res.render("pages/db", data);
 });
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.post("/adduser", (req, res) => {
+  console.log("post request for /adduser");
+  var uname = req.body.uname;
+  var age = req.body.age;
+  res.send(`username: ${uname}, age: ${age}`);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
