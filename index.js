97895a37e722b0442899bb368bbfffe9eb209201
William Ting <williamdjting@gmail.com>
2022-06-18 16:43:12 -0700

    added app.post to /adduser, need to fix the getUsersQuery string interpolation

--- index.js
+++ index.js
@@ -35,17 +35,46 @@ app.get("/database", (req, res) => {
   });
 });
 
+app.get("/adduser", (req, res) => {
+  res.render("pages/addUser");
+});
+
 app.post("/adduser", (req, res) => {
   console.log("post request for /adduser");
-  var uname = req.body.uname;
-  var age = req.body.age;
-  // res.send(`usersname: ${uname}, age: ${age}`);
-  var addUserData = { userName: { uname }, userAge: { age } };
-  console.log("addUserData object", addUserData);
-  console.log("addUserData.userName object", addUserData.userName);
-  console.log("addUserData.userAge object", addUserData.userAge);
-  res.render("pages/addUser", addUserData);
+  var name = req.body.name;
+  var weight = req.body.weight;
+  var height = req.body.height;
+  var haircolor = req.body.haircolor;
+  var gpa = req.body.gpa;
+  // var getUsersQuery = `INSERT INTO student (name, weight, height, haircolor, gpa) VALUES ('${name}','${weight}','${height}','${haircolor}','${gpa}')`;
+  var getUsersQuery = "INSERT INTO student VALUES ('will2', 125,100,'red',2.5)";
+  // var getUsersQuery = "INSERT INTO student (name, weight, height, hcolor, gpa) VALUES ('" + name + "', " + weight + " , " + height + ",'" + haircolor + "', " + gpa + ")";
+
+  console.log("post request for getUsersQuery");
+  pool.query(getUsersQuery, (error, result) => {
+    if (error) {
+      res.send(error);
+    }
+    console.log("post request for pool.query");
+    // var results = { 'rows': result.rows };
+    // res.render("pages/db", results);
+    res.redirect("/database");
+  });
+  console.log("post request for after getUsersQuery");
+
 });
+
+// app.post("/adduser", (req, res) => {
+//   console.log("post request for /adduser");
+//   var uname = req.body.uname;
+//   var age = req.body.age;
+//   // res.send(`usersname: ${uname}, age: ${age}`);
+//   var addUserData = { userName: { uname }, userAge: { age } };
+//   console.log("addUserData object", addUserData);
+//   console.log("addUserData.userName object", addUserData.userName);
+//   console.log("addUserData.userAge object", addUserData.userAge);
+//   res.render("/database");
+// });
 
 app.get("/users/:id", (req, res) => {
   var uid = req.params.id;
