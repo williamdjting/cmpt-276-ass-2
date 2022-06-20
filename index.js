e5cc0bbcc3cb1f4c4d25072ab6d27e14f2c0b047
William Ting <williamdjting@gmail.com>
2022-06-19 21:53:10 -0700

    add database to heroku via add on

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const { Pool } = require("pg");
 //   connectionString: `postgres://postgres:${psqlPassword}@localhost/student`,
 // });
 var pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgres://postgres:root@localhost/cmpt276",
+  // connectionString: process.env.DATABASE_URL || "postgres://postgres:root@localhost/cmpt276",
+  connectionString: process.env.DATABASE_URL
 ssl: {
   rejectUnauthorized: false
   }
